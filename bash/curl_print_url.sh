d29760d4fcbf77fd7e26019f32672a6b8ab8d5fc
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-08-22 16:33:34 +0300

    Update curl_print_url.sh

--- bash/curl_print_url.sh
+++ bash/curl_print_url.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#define organization and box variables
+# Define organisation and box variables
 echo "Specify user:"
 read var1
 echo "Specify box:"
 read var2
 
-#Exports URLs of all available providers for the box
+# Exports URLs of all available providers for the box
 curl -sL  https://app.vagrantup.com/api/v1/box/$var1/$var2 | jq '.current_version.providers[].download_url'
